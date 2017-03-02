874ae3620a94e204bbfa8b631c794faa4cfac03b
Joshua Bennett <josh.bennett@mun.ca>
2017-03-02 19:39:02 -0330

    Changed gridview to grid layout

--- DesignProject/app/src/main/java/joshbennett/designproject/LevelSelectActivity.java
+++ DesignProject/app/src/main/java/joshbennett/designproject/LevelSelectActivity.java
@@ -19,69 +19,39 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LevelSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setContentView(R.layout.activity_level_select);
 
         //LayoutParams params = new LinearLayout().LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        ArrayList<ImageButton> buttons = new ArrayList<>();
 
-        GridView grid = (GridView) findViewById(R.id.grid);
-        grid.setAdapter(new ImageAdapter(this));
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?>parent, View v, int position, long id){
-                Toast.makeText(LevelSelectActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        });
-
-    }
-
-}
-
-class ImageAdapter extends BaseAdapter {
-    private Context mContext;
-
-    public ImageAdapter(Context c) {
-        mContext = c;
-    }
-
-    public int getCount() {
-        return mThumbIds.length;
-    }
-
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(LayoutParams.MATCH_PARENT, 100));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+        for (int i = 0; i < 12; i++) {
+            final int j = i;
+            ImageButton level = new ImageButton(this);
+            level.setImageResource(R.drawable.penis);
+            level.setsca
+            level.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(LevelSelectActivity.this, ((CharSequence)("button" + j)), Toast.LENGTH_SHORT).show();
+                }
+            });
+            buttons.add(level);
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.penis, R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis,R.drawable.penis
-    };
+        for(int i = 0; i < 12; i++) {
+            grid.addView(buttons.get(i));
+        }
+    }
 }
